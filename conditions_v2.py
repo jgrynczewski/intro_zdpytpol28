7a5d16be0711540578b4b8ed3954fbf28bf28867
jgrynczewski <Tel^)%322))$>
2022-10-12 19:07:20 +0200

    introduce conditions

---
+++ conditions_v2.py
@@ -0,0 +1,8 @@
+age = int(input("Proszę podać swój wiek: "))
+
+if age >= 18:
+    print("Możesz wejść")
+elif age >= 16:
+    print("Sorry, jesteś niepełnoletni")
+else:
+    print("Sorry, jesteś nieletni")
